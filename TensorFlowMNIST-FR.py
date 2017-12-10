cacb9ea8041720475341a1ddb7c2cd9792e5ec0d
Houssam <chmourihoussam@gmail.com>
2017-12-10 13:17:13 +0000

    Update TensorFlowMNIST-FR.py

--- TensorFlowMNIST-FR.py
+++ TensorFlowMNIST-FR.py
@@ -1,4 +1,3 @@
-"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
 """Réseau de neurones convolutifs pour le dataset MNIST, construit avec tf.layers."""
 
 from __future__ import absolute_import
@@ -11,18 +10,23 @@ import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.INFO)
 
 def cnn_model_fn(features, labels, mode):
-  """Model function for CNN."""
-  # Input Layer
-  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-  # MNIST images are 28x28 pixels, and one greyscale color channel, hence [-1, 28, 28, 1]
-  # the "-1" bacth size is to indicate the dynamically aspect of this dimension
+  """Construire l'architecture du réseau (ou model)"""
+  # Couche d'input
+  # features contient un paramètre appelé "x" qui représente nos features (i.e.: les images)
+  # Transformer x en un tensor 4-D: [batch_size, width, height, channels]
+  # Les images du dataset MNIST sont 28x28 pixels, noir et blanc, donc une seule valeur dans chaque pixel, 
+  # d'où [-1, 28, 28, 1]
+  # La première dimension (ici "-1") est pour indiquer le nombre de "features" dans le tensor (batch size en anglais), 
+  # nous passons "-1" pour signifier que le nombre est dynamique
   input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
-  # Convolutional Layer #1
-  # Computes 32 features using a 5x5 filter with ReLU activation.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 28, 28, 1]
-  # Output Tensor Shape: [batch_size, 28, 28, 32]
+  # Couche de convolution #1
+  # Effectue 32 convolutions (donc 32 filtres, ou "kernels"), de dimension 5x5 pour chaque filtre
+  # La couche de convolution est, généralement suivie par une "couche" qui applique une fonction d'activation
+  # sur l'output de lacouche de convolution, ici nous utiliserons l'actifaion ReLU (Rectified Linear Unit)
+  # Nous utiliserons le "same padding". Cela, pour conserver les dimension d'hauteur et largeur de la convolution précédente
+  # Le tensor d'input est de dimension : [batch_size, 28, 28, 1]
+  # Le tensor d'output (conv1) est de dimension : [batch_size, 28, 28, 32]
   conv1 = tf.layers.conv2d(
       inputs=input_layer,
       filters=32,
@@ -30,17 +34,17 @@ def cnn_model_fn(features, labels, mode):
       padding="same",
       activation=tf.nn.relu)
 
-  # Pooling Layer #1
-  # First max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 28, 28, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 32]
+  # Couche de convolution #1
+  # Nous utiliserons une couche de max-pooling avec un filtre de dimension 2x2 et un pas ("stride" en anglais) de 2
+  # Le tensor d'input est de dimension : [batch_size, 28, 28, 32]
+  # Le tensor d'output (pool1) est de dimension : [batch_size, 14, 14, 32]
   pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
   # Convolutional Layer #2
-  # Computes 64 features using a 5x5 filter.
-  # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 14, 14, 32]
-  # Output Tensor Shape: [batch_size, 14, 14, 64]
+  # Effectue 64 convolutions de dimension 5x5 pour chaque filtre.
+  # Nous utiliserons le "same padding".
+  # Le tensor d'input est de dimension : [batch_size, 14, 14, 32]
+  # Le tensor d'output (conv2) est de dimension : [batch_size, 14, 14, 64]
   conv2 = tf.layers.conv2d(
       inputs=pool1,
       filters=64,
@@ -48,62 +52,65 @@ def cnn_model_fn(features, labels, mode):
       padding="same",
       activation=tf.nn.relu)
 
-  # Pooling Layer #2
-  # Second max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 14, 14, 64]
-  # Output Tensor Shape: [batch_size, 7, 7, 64]
+  # Couche de pooling #2
+  # Nous utiliserons une couche de max-pooling avec un filtre de dimension 2x2 et un pas ("stride" en anglais) de 2
+  # Le tensor d'input est de dimension : [batch_size, 14, 14, 64]
+  # Le tensor d'output (pool2) est de dimension : [batch_size, 7, 7, 64]
   pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-  # Convolutional Layer #3
-  # Computes 128 features using a 3x3 filter.
+  # Couche de convolution #3
+  # Effectue 128 convolutions de dimension 3x3 pour chaque filtre.
   # Padding is added to preserve width and height.
-  # Input Tensor Shape: [batch_size, 7, 7, 64]
-  # Output Tensor Shape: [batch_size, 7, 7, 128]
+  # Le tensor d'input est de dimension : [batch_size, 7, 7, 64]
+  # Le tensor d'output (conv3) est de dimension : [batch_size, 7, 7, 128]
   conv3 = tf.layers.conv2d(
-      inputs=pool1,
+      inputs=pool2,
       filters=128,
       kernel_size=[3, 3],
       padding="same",
       activation=tf.nn.relu)
-    
-  pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
+  
+  # Couche de pooling #3
+  # Nous utiliserons une couche de max-pooling avec un filtre de dimension 2x2 et un pas ("stride" en anglais) de 2
+  # Le tensor d'input est de dimension : [batch_size, 7, 7, 128]
+  # Le tensor d'output (pool2) est de dimension : [batch_size, 3, 3, 128]
+  pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[3, 3], strides=2)
 
-  # Flatten tensor into a batch of vectors
-  # Input Tensor Shape: [batch_size, 7, 7, 128]
-  # Output Tensor Shape: [batch_size, 7 * 7 * 128]
-  pool3_flat = tf.reshape(pool3, [-1, 7 * 7 * 128])
+  # Nous transformons la dimension du tensor précdent (bacth_size, 3, 3, 128) 
+  # en un tensor 2D de dimension (batch_size, 3 * 3 * 128)
+  # Le tensor d'input est de dimension : [batch_size, 3, 3, 128]
+  # Le tensor d'output (pool3_flat) est de dimension : [batch_size, 3 * 3 * 128]
+  pool3_flat = tf.reshape(pool3, [-1, 3 * 3 * 128])
 
-  # Dense Layer
-  # Densely connected layer with 1024 neurons
-  # Input Tensor Shape: [batch_size, 7 * 7 * 64]
-  # Output Tensor Shape: [batch_size, 1024]
+  # Couche dense (Fully-connected)
+  # Couche dense de 1024 neurones (batch_size, 1024)
+  # Le tensor d'input est de dimension : [batch_size, 3 * 3 * 128]
+  # Le tensor d'output (dense) est de dimension : [batch_size, 1024]
   dense = tf.layers.dense(inputs=pool3_flat, units=1024, activation=tf.nn.relu)
 
-  # Add dropout operation; 0.6 probability that element will be kept
+  # Couche dropout; 1 - 0.4 = 0.6 est la probabilité que le "noeud" soit gardé
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-  # Logits layer
-  # Input Tensor Shape: [batch_size, 1024]
-  # Output Tensor Shape: [batch_size, 10]
+  # Couche logits
+  # Le tensor d'input est de dimension : [batch_size, 1024]
+  # Le tensor d'output (logits) est de dimension : [batch_size, 10] (pour signifier les 10 classes, de 0 à 9)
   logits = tf.layers.dense(inputs=dropout, units=10)
 
   predictions = {
-      # Generate predictions (for PREDICT and EVAL mode)
+      # Generer les predictions (pour les modes PREDICT et EVAL)
       "classes": tf.argmax(input=logits, axis=1),
-      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-      # `logging_hook`.
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
   }
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-  # Calculate Loss (for both TRAIN and EVAL modes)
+  # Calculer le Loss (perte) (pour les modes TRAIN et EVAL)
   onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
   loss = tf.losses.softmax_cross_entropy(
       onehot_labels=onehot_labels, logits=logits)
 
-  # Configure the Training Op (for TRAIN mode)
+  # Configurer l'opération d'entrainement (pour le mode TRAIN)
   if mode == tf.estimator.ModeKeys.TRAIN:
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
     train_op = optimizer.minimize(
@@ -111,7 +118,7 @@ def cnn_model_fn(features, labels, mode):
         global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
-  # Add evaluation metrics (for EVAL mode)
+  # Calculer les metrics d'évaluation (pour le mode EVAL)
   eval_metric_ops = {
       "accuracy": tf.metrics.accuracy(
           labels=labels, predictions=predictions["classes"])}
@@ -119,24 +126,25 @@ def cnn_model_fn(features, labels, mode):
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
       
 def main(unused_argv):
-  # Load training and eval data
+  # Charger les données d'entrainement et d'évaluation
   mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-  train_data = mnist.train.images  # Returns np.array
+  train_data = mnist.train.images  # de type np.array
   train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-  eval_data = mnist.test.images  # Returns np.array
+  eval_data = mnist.test.images  # de type np.array
   eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
-  # Create the Estimator
+  # Créer l'estimateur (Estimator)
   mnist_classifier = tf.estimator.Estimator(
       model_fn=cnn_model_fn, model_dir=None)
 
-  # Set up logging for predictions
-  # Log the values in the "Softmax" tensor with label "probabilities"
-  #tensors_to_log = {"probabilities": "softmax_tensor"}
-  #logging_hook = tf.train.LoggingTensorHook(
-  #    tensors=tensors_to_log, every_n_iter=50)
+  # Optionnel
+  # Préparer la journalisation pour les prédictions
+  # Ajouter le tensor "Softmax" dans la journalisation (avec un key "probabilities")
+  # tensors_to_log = {"probabilities": "softmax_tensor"}
+  # logging_hook = tf.train.LoggingTensorHook(
+  #   tensors = tensors_to_log, every_n_iter=50)
 
-  # Train the model
+  # Commencer l'entrainement
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": train_data},
       y=train_labels,
@@ -150,7 +158,7 @@ def main(unused_argv):
       #hooks=[logging_hook]
       )
 
-  # Evaluate the model and print results
+  # Evaluer le modèle et afficher les résultats
   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": eval_data},
       y=eval_labels,
